8770f31165dfe004fac4ed778efb9a111acfa0a4
Whose-v2 <123986403+Whose-v2@users.noreply.github.com>
2023-01-31 16:09:10 +0300

    Update router.php

--- settings/router.php
+++ settings/router.php
@@ -4,9 +4,9 @@
 //$password = Database şifre
 //buna göre düzelt
 
-$dbname = 'shh';
-$user = 'root';
-$password = '';
+$dbname = 'heroku_f04f91a6ea1636d';
+$user = 'b6c6f29f9c96bf';
+$password = '356cdcc5';
 $dsn = 'mysql:host=localhost;charset=utf8;dbname='.$dbname;
 
 ?>
